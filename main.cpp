f40675b62b49ed93aa122d0e3948e75617717291
winexe0 <aryan.chandna@icloud.com>
2021-10-22 18:14:58 -0700

    Revert "Mention the license"
    This reverts commit 4304f59dd04ff23fbae0ff2d892de92d5d033383.

--- main.cpp
+++ main.cpp
@@ -34,18 +34,6 @@ int main(int argc, char **argv)
 	int bedtimeforwake;
 	int bedtime24hr;
 	int wakeuptime2;
-	ht_printf("This program is free software; you can redistribute it and/or modify\n"
-			"it under the terms of the GNU General Public License version 2 as published by\n"
-			"the Free Software Foundation.\n"
-			"\n"
-			"This program is distributed in the hope that it will be useful,\n"
-			"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-			"GNU General Public License for more details.\n"
-			"\n"
-			"You should have received a copy of the GNU General Public License\n"
-			"along with this program; if not, write to the Free Software\n"
-			"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA\n\n");
 	cout << "Do you want to calcuate the bedtime or the wakeup time. Bedime or Wakeup?" << endl;
 	cin >> timetype;
 	if (timetype == "Bedtime" || timetype == "bedtime") {
